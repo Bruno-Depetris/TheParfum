894a9cefbebb634d48ea04053c929d6fc116bd62
Bruno-Depetris <Depetrisbruno@gmail.com>
2025-01-16 00:37:45 +0100

    Agregar archivos de proyecto.

---
+++ TheParfum/Formularios/FormularioPrincipal.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace TheParfum {
+    partial class FormularioPrincipal {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormularioPrincipal));
+            this.ThePerfum = new ReaLTaiizor.Forms.HopeForm();
+            this.panel_ContenedorLateral = new System.Windows.Forms.Panel();
+            this.parrotGradientPanel1 = new ReaLTaiizor.Controls.ParrotGradientPanel();
+            this.panel_Contenedor = new System.Windows.Forms.Panel();
+            this.button_Venta = new ReaLTaiizor.Controls.Button();
+            this.button1 = new ReaLTaiizor.Controls.Button();
+            this.button2 = new ReaLTaiizor.Controls.Button();
+            this.button3 = new ReaLTaiizor.Controls.Button();
+            this.panel_ContenedorLateral.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ThePerfum
+            // 
+            this.ThePerfum.ControlBoxColorH = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(231)))), ((int)(((byte)(237)))));
+            this.ThePerfum.ControlBoxColorHC = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(108)))), ((int)(((byte)(108)))));
+            this.ThePerfum.ControlBoxColorN = System.Drawing.Color.White;
+            this.ThePerfum.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ThePerfum.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ThePerfum.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ThePerfum.ForeColor = System.Drawing.Color.White;
+            this.ThePerfum.Image = null;
+            this.ThePerfum.Location = new System.Drawing.Point(0, 0);
+            this.ThePerfum.Name = "ThePerfum";
+            this.ThePerfum.Size = new System.Drawing.Size(1006, 40);
+            this.ThePerfum.TabIndex = 0;
+            this.ThePerfum.Text = "ThePerfum";
+            this.ThePerfum.ThemeColor = System.Drawing.Color.Black;
+            // 
+            // panel_ContenedorLateral
+            // 
+            this.panel_ContenedorLateral.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel_ContenedorLateral.BackgroundImage")));
+            this.panel_ContenedorLateral.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel_ContenedorLateral.Controls.Add(this.button3);
+            this.panel_ContenedorLateral.Controls.Add(this.button2);
+            this.panel_ContenedorLateral.Controls.Add(this.button1);
+            this.panel_ContenedorLateral.Controls.Add(this.button_Venta);
+            this.panel_ContenedorLateral.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel_ContenedorLateral.Location = new System.Drawing.Point(0, 40);
+            this.panel_ContenedorLateral.Name = "panel_ContenedorLateral";
+            this.panel_ContenedorLateral.Size = new System.Drawing.Size(224, 599);
+            this.panel_ContenedorLateral.TabIndex = 1;
+            // 
+            // parrotGradientPanel1
+            // 
+            this.parrotGradientPanel1.BottomLeft = System.Drawing.Color.Black;
+            this.parrotGradientPanel1.BottomRight = System.Drawing.Color.Fuchsia;
+            this.parrotGradientPanel1.CompositingQualityType = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            this.parrotGradientPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.parrotGradientPanel1.InterpolationType = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+            this.parrotGradientPanel1.Location = new System.Drawing.Point(224, 40);
+            this.parrotGradientPanel1.Name = "parrotGradientPanel1";
+            this.parrotGradientPanel1.PixelOffsetType = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            this.parrotGradientPanel1.PrimerColor = System.Drawing.Color.White;
+            this.parrotGradientPanel1.Size = new System.Drawing.Size(5, 599);
+            this.parrotGradientPanel1.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            this.parrotGradientPanel1.Style = ReaLTaiizor.Controls.ParrotGradientPanel.GradientStyle.Vertical;
+            this.parrotGradientPanel1.TabIndex = 2;
+            this.parrotGradientPanel1.TextRenderingType = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.parrotGradientPanel1.TopLeft = System.Drawing.Color.Gray;
+            this.parrotGradientPanel1.TopRight = System.Drawing.Color.White;
+            // 
+            // panel_Contenedor
+            // 
+            this.panel_Contenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_Contenedor.Location = new System.Drawing.Point(229, 40);
+            this.panel_Contenedor.Name = "panel_Contenedor";
+            this.panel_Contenedor.Size = new System.Drawing.Size(777, 599);
+            this.panel_Contenedor.TabIndex = 3;
+            // 
+            // button_Venta
+            // 
+            this.button_Venta.BackColor = System.Drawing.Color.Transparent;
+            this.button_Venta.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.button_Venta.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_Venta.EnteredBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button_Venta.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.button_Venta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button_Venta.Image = null;
+            this.button_Venta.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_Venta.InactiveColor = System.Drawing.Color.Transparent;
+            this.button_Venta.Location = new System.Drawing.Point(0, 125);
+            this.button_Venta.Name = "button_Venta";
+            this.button_Venta.PressedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button_Venta.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button_Venta.Size = new System.Drawing.Size(226, 40);
+            this.button_Venta.TabIndex = 2;
+            this.button_Venta.Text = "Ventas";
+            this.button_Venta.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.button_Venta.Click += new System.EventHandler(this.button_Venta_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.EnteredBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button1.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Image = null;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.button1.Location = new System.Drawing.Point(0, 171);
+            this.button1.Name = "button1";
+            this.button1.PressedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button1.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button1.Size = new System.Drawing.Size(226, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ventas";
+            this.button1.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.EnteredBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button2.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button2.Image = null;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.button2.Location = new System.Drawing.Point(0, 217);
+            this.button2.Name = "button2";
+            this.button2.PressedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button2.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button2.Size = new System.Drawing.Size(226, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Ventas";
+            this.button2.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.EnteredBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button3.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button3.Image = null;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.button3.Location = new System.Drawing.Point(0, 263);
+            this.button3.Name = "button3";
+            this.button3.PressedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button3.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button3.Size = new System.Drawing.Size(226, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Ventas";
+            this.button3.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // FormularioPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 639);
+            this.Controls.Add(this.panel_Contenedor);
+            this.Controls.Add(this.parrotGradientPanel1);
+            this.Controls.Add(this.panel_ContenedorLateral);
+            this.Controls.Add(this.ThePerfum);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(1600, 900);
+            this.MinimumSize = new System.Drawing.Size(190, 40);
+            this.Name = "FormularioPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel_ContenedorLateral.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Forms.HopeForm ThePerfum;
+        private System.Windows.Forms.Panel panel_ContenedorLateral;
+        private ReaLTaiizor.Controls.ParrotGradientPanel parrotGradientPanel1;
+        private System.Windows.Forms.Panel panel_Contenedor;
+        private ReaLTaiizor.Controls.Button button_Venta;
+        private ReaLTaiizor.Controls.Button button3;
+        private ReaLTaiizor.Controls.Button button2;
+        private ReaLTaiizor.Controls.Button button1;
+    }
+}
+
---
+++ TheParfum/Formularios/FormularioPrincipal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheParfum {
+    public partial class FormularioPrincipal : Form {
+        public FormularioPrincipal() {
+            InitializeComponent();
+        }
+
+        private Form FormActivo = null;
+
+        private void AbrirFormNuevo(Form FormHijo) {
+            // Cerrar el formulario activo anterior
+            if (FormActivo != null) {
+                FormActivo.Close();
+            }
+
+            // Configurar el nuevo formulario
+            FormActivo = FormHijo;
+            FormHijo.TopLevel = false;
+            FormHijo.FormBorderStyle = FormBorderStyle.None;
+            FormHijo.Dock = DockStyle.Fill;
+            panel_Contenedor.Controls.Add(FormHijo);
+            panel_Contenedor.Tag = FormHijo;
+            FormHijo.Show();
+        }
+
+        private void button_Venta_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
